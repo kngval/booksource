d5dcbd4458607fdd92cf564e8eade341a9e6ee2d
kngval <bkvalteocs@gmail.com>
2025-03-06 12:51:31 +0000

    bg-color

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,25 @@
-import { useTheme } from "../theme/themeContext";
-import { Text, View } from "react-native";
+import { Theme, useTheme } from "../theme/themeContext";
+import { StyleSheet, Text, View } from "react-native";
+
 
 export default function Index() {
-  const { theme,toggleTheme } = useTheme();
+  const { theme } = useTheme();
+  const styles = getStyles(theme);
   return (
-    <View style={{backgroundColor : theme.background}}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.screenWrapper}>
+      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
+}
+
+const getStyles = (theme: Theme) => {
+  return StyleSheet.create({
+    screenWrapper: {
+      backgroundColor: theme.background,
+      height:"100%"
+    },
+    text: {
+      color: theme.text
+    }
+  })
 }
