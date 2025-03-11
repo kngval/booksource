d12e13773d23718aeace3c756e82724e88fdf5d5
kngval <bkvalteocs@gmail.com>
2025-03-11 20:06:55 +0000

    user tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,22 +1,24 @@
 import BookMarkIcon from "@/assets/bookmarkSvg";
 import FilterSvg from "@/assets/filterSvg";
 import HomeIcon from "@/assets/homeSvg";
+import UserIcon from "@/assets/userSvg";
 import { useTheme } from "@/theme/themeContext";
 import { Tabs } from "expo-router";
-import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { View, StyleSheet, TouchableOpacity, Platform } from "react-native";
 export default function TabsLayout() {
   const { theme } = useTheme();
   return (
     <Tabs screenOptions={{
       tabBarStyle: {
-        width:340,
+        width:330,
         height:80,
         backgroundColor: theme.nav,
         borderTopLeftRadius: 28,
         borderTopRightRadius: 28,
         alignSelf: "center",
+        alignItems: "center",
         flexDirection : "row",
-        paddingTop:20
+        paddingBottom:Platform.OS === "android" ? 20 : 0
       },
 
     }}>
@@ -29,7 +31,7 @@ export default function TabsLayout() {
         },
         tabBarLabel: "",
         tabBarIcon: () => (
-          <HomeIcon width={30} height={30} color={theme.background} />
+          <HomeIcon width={25} height={30} color={theme.background} />
         )
         ,
         headerStyle: {
@@ -53,8 +55,9 @@ export default function TabsLayout() {
       }} />
 
 
-      <Tabs.Screen name="bookmarks" options={{
-        title: "Book Marks",
+
+      <Tabs.Screen name="user" options={{
+        title: "User",
         headerTitleStyle: {
           fontWeight: 700,
           fontSize: 25,
@@ -62,7 +65,7 @@ export default function TabsLayout() {
         },
         tabBarLabel: "",
         tabBarIcon: () => (
-          <BookMarkIcon width={100} height={25} color={theme.background} />
+          <UserIcon width={30} height={30} color={theme.background} />
         )
         ,
         headerStyle: {
@@ -73,16 +76,29 @@ export default function TabsLayout() {
           shadowColor: "transparent",
           borderBottomWidth: 0,
         },
-        headerRight: () => (
-          <View style={styles.rightContainer}>
-            <FilterSvg width={25} height={25} color={theme.text} />
-            <TouchableOpacity style={styles.menuContainer}>
-              <View style={{ ...styles.menu, backgroundColor: theme.text }}></View>
-              <View style={{ ...styles.menu, backgroundColor: theme.text }}></View>
-              <View style={{ ...styles.menu, backgroundColor: theme.text }}></View>
-            </TouchableOpacity>
-          </View>
-        ),
+      }} />
+
+
+      <Tabs.Screen name="bookmarks" options={{
+        title: "Book Marks",
+        headerTitleStyle: {
+          fontWeight: 700,
+          fontSize: 25,
+          color: theme.text,
+        },
+        tabBarLabel: "",
+        tabBarIcon: () => (
+          <BookMarkIcon width={30} height={30} color={theme.background} />
+        )
+        ,
+        headerStyle: {
+          backgroundColor: theme.background,
+          borderWidth: 0,
+          elevation: 0,
+          shadowOpacity: 0,
+          shadowColor: "transparent",
+          borderBottomWidth: 0,
+        },
       }} />
     </Tabs>
   )
---
+++ app/(tabs)/user.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import { Text } from "react-native-svg";
+
+
+export default function UserScreen(){
+
+  return (
+    <View>
+      <Text>HELLO USER</Text>
+    </View>
+  )
+}
