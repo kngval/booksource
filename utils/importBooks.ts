bff4ff96eb201ebb9f3e94bb52ffdc578f962c62
kngval <bkvalteocs@gmail.com>
2025-04-13 01:49:02 +0800

    refactor

--- utils/importBooks.ts
+++ utils/importBooks.ts
@@ -1,21 +1,33 @@
 import * as FileSystem from "expo-file-system";
 import { TBookMetaData } from "@/types/book.types";
-import { v4 as uuidv4 } from 'uuid';
+import { getBookData } from "./getBookData";
+import * as Crypto from "expo-crypto";
+export const importBook = async (bookUri: string):Promise<TBookMetaData> => {
 
-export const importBook = async (bookUri: string, bookMetaData: TBookMetaData) => {
+  const id = Crypto.randomUUID();
 
-  const id = uuidv4();
+  const book = await getBookData(bookUri);
+
+  //cover
+  const cover = await book.loaded.cover;
+  const coverBase64 = await book.archive.getBase64(cover);
+
+  //metadata
+  const metadata = await book.loaded.metadata;
+  //uuid
   const newPath = `${FileSystem.documentDirectory}${id}.epub`
   console.log("New Path : ", newPath);
   await FileSystem.copyAsync({
     from: bookUri,
     to: newPath
   })
-  const newBook:TBookMetaData = {
-    ...bookMetaData,
-    path : newPath
+
+  const newBook: TBookMetaData = {
+    ...metadata,
+    id: id,
+    path: newPath,
+    cover: coverBase64
   }
 
-  console.log("NEW Book metadata : ", newBook);
   return newBook;
 }
