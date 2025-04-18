8267d671d39b745830c13be58b43f9fdc71983eb
kngval <bkvalteocs@gmail.com>
2025-04-19 03:30:55 +0800

    route push to /books/{book.id}

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,10 +2,12 @@ import { useLibrary } from "@/books/BookContext";
 import { useTheme } from "@/theme/themeContext";
 import { TBookMetaData } from "@/types/book.types";
 import { useEffect } from "react";
-import { FlatList, Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { FlatList, Image, Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import { useRouter } from "expo-router";
 export default function HomeScreen() {
   const { theme } = useTheme();
   const { library, loadLibrary, deleteBook } = useLibrary();
+  const router = useRouter();
   useEffect(() => {
     // deleteBook();
 
@@ -14,21 +16,23 @@ export default function HomeScreen() {
 
   const renderBook = ({ item }: { item: TBookMetaData }) => {
     return (
-      <View style={{ elevation: 2, borderWidth: 0.1, marginHorizontal: 2, alignItems: "center",paddingBottom:10 }}>
-        {item.cover && (
-          <Image
-            source={{ uri: item.cover }}
-            style={{ width: 110, height: 160 }}
-          />
-        )}
+      <Pressable onPress={() => router.push(`/books/${item.id}`)} style={{ elevation: 2, borderWidth: 0.1, marginHorizontal: 2, alignItems: "center", paddingBottom: 10 }}>
+        <View >
+          {item.cover && (
+            <Image
+              source={{ uri: item.cover }}
+              style={{ width: 110, height: 160 }}
+            />
+          )}
 
-        <Text style={{ color: theme.text, width: 100, textAlign: "center", fontWeight: 600, marginVertical: 10, flexWrap: "wrap" }}>{item.title}</Text>
-      </View>
+          <Text style={{ color: theme.text, width: 100, textAlign: "center", fontWeight: 600, marginVertical: 10, flexWrap: "wrap" }}>{item.title}</Text>
+        </View>
+      </Pressable>
     );
   }
 
   return (
-    <View style={{ width:"100%",height:"100%", backgroundColor: theme.background,paddingHorizontal:5,alignItems:"center" }}>
+    <View style={{ width: "100%", height: "100%", backgroundColor: theme.background, paddingHorizontal: 5, alignItems: "center" }}>
       <FlatList
         data={library}
         keyExtractor={(item) => item.id}
