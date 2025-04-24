7f208e6a00f483a3373d5fcfff4a3a28f997c81b
kngval <bkvalteocs@gmail.com>
2025-04-24 22:06:07 +0800

    router push

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,32 +2,34 @@ import { useLibrary } from "@/books/BookContext";
 import { useTheme } from "@/theme/themeContext";
 import { TBookMetaData } from "@/types/book.types";
 import { useEffect } from "react";
-import { FlatList, Image, Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
-import { useRouter } from "expo-router";
+import { FlatList, Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { Link, useRouter } from "expo-router";
 export default function HomeScreen() {
   const { theme } = useTheme();
-  const { library, loadLibrary, deleteBook } = useLibrary();
+  const { library, loadLibrary } = useLibrary();
   const router = useRouter();
   useEffect(() => {
     // deleteBook();
-
     loadLibrary();
   }, []);
 
   const renderBook = ({ item }: { item: TBookMetaData }) => {
     return (
-      <Pressable onPress={() => router.push(`/books/${item.id}`)} style={{ alignItems: "center", paddingBottom: 10, marginHorizontal:8}}>
+      <Link 
+        href={`/books/${item.id}`}
+        style={{ alignItems: "center", paddingBottom: 10, marginHorizontal:8}}>
+
         <View style={{ alignItems:"center" }}>
           {item.cover && (
             <Image
               source={{ uri: item.cover }}
-              style={{ width: 100, height: 170,borderRadius:5,borderColor:theme.text,borderWidth:2 }}
+              style={{ width: 100, height: 170,borderRadius:5 }}
             />
           )}
 
           <Text style={{ color: theme.text, width: 100, textAlign: "center",fontSize:12, fontWeight: 700, marginTop:8,flexWrap: "wrap" }}>{item.title}</Text>
         </View>
-      </Pressable>
+      </Link>
     );
   }
 
