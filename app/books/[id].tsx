0d1aaba7467a94407b58da196ea6567116bb514e
kngval <bkvalteocs@gmail.com>
2025-04-24 22:06:27 +0800

    book details l/d mode, and placeholder

--- app/books/[id].tsx
+++ app/books/[id].tsx
@@ -1,54 +1,51 @@
 import { useLibrary } from "@/books/BookContext";
-import { useTheme } from "@/theme/themeContext";
+import { lightTheme, useTheme } from "@/theme/themeContext";
 import { TBookMetaData } from "@/types/book.types";
+import { useNavigation } from "@react-navigation/native";
 import { useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
-import { Image, ScrollView, Text, useWindowDimensions, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import HTMLView from "react-native-htmlview"
 export default function BookDetails() {
   const [book, setBook] = useState<TBookMetaData | null>(null);
   const { theme } = useTheme();
-  const { id } = useLocalSearchParams();
   const { library } = useLibrary();
+  const { id } = useLocalSearchParams();
 
   useEffect(() => {
     loadBook();
-  }, [])
+  },[])
 
   const loadBook = () => {
-    const book = library.find(b => b.id === id);
-    if (book) {
-      console.log("info : ", book.title, book.cover);
-      setBook(book);
+    const b = library.find(b => b.id === id);
+    if(b){
+      setBook(b);
     } else {
-      throw new Error(`No book found with id : ${id}`)
+      setBook(null);
     }
   }
 
   return (
-    <ScrollView style={{ backgroundColor: theme.background }}>
+    <ScrollView style={{ backgroundColor: theme.background,minHeight:"100%" }}>
       {book ? (
         <>
           <View style={{ alignItems: "center", marginTop: 50, marginBottom: 80 }}>
             <Image
               source={{ uri: book.cover }}
-              style={{ width: 180, height: 300,borderRadius:10,borderWidth:2,borderColor:theme.nav }}
+              style={{ width: 180, height: 300,borderRadius:5 }}
             />
           </View>
 
           <View style={{ minHeight: "100%", backgroundColor: theme.nav, borderTopLeftRadius: 30, borderTopRightRadius: 30, padding: 30 }}>
-            <Text style={{ color: theme.background, fontSize: 22, fontWeight: 900 }}>{book.title}</Text>
-            <Text style={{ color: theme.background, fontSize: 15, marginBottom: 20 }}>{book.creator}</Text>
+            <Text style={{ color: theme.background == "#F2EAC5" ? theme.background: theme.text, fontSize: 22, fontWeight: 900 }}>{book.title}</Text>
+            <Text style={{ color: theme.background == "#F2EAC5" ? theme.background: theme.text, fontSize: 15, marginBottom: 20,fontStyle:"italic" }}> - {book.creator}</Text>
 
 
-            <Text style={{ fontSize:18,color:theme.background,fontWeight:700,marginBottom:5 }}>Description</Text>
+            <Text style={{ fontSize:18,color:theme.background == "#F2EAC5" ? theme.background: theme.text,fontWeight:700,marginBottom:5 }}>Description</Text>
             <HTMLView
-              value={`<div>${book.description}</div>`}
+              value={`<div>${book.description.length > 0 ? book.description : "There are no description available for this book"}</div>`}
               stylesheet={{
-                p: {
-                  color: theme.background
-                },
-                div: { color: theme.background },
+                div: { color: theme.background ==  "#F2EAC5"? theme.background : theme.text },
               }}
             />
           </View>
