7319e8b4ddbfb1115a90090d6c078e8be0bd8162
kngval <bkvalteocs@gmail.com>
2025-04-26 10:16:29 +0800

    reading screen

--- app/books/[id].tsx
+++ app/books/[id].tsx
@@ -4,7 +4,7 @@ import { TBookMetaData } from "@/types/book.types";
 import { useNavigation } from "@react-navigation/native";
 import { useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, Pressable, ScrollView, Text, View } from "react-native";
 import HTMLView from "react-native-htmlview"
 export default function BookDetails() {
   const [book, setBook] = useState<TBookMetaData | null>(null);
@@ -39,6 +39,10 @@ export default function BookDetails() {
           <View style={{ minHeight: "100%", backgroundColor: theme.nav, borderTopLeftRadius: 30, borderTopRightRadius: 30, padding: 30 }}>
             <Text style={{ color: theme.background == "#F2EAC5" ? theme.background: theme.text, fontSize: 22, fontWeight: 900 }}>{book.title}</Text>
             <Text style={{ color: theme.background == "#F2EAC5" ? theme.background: theme.text, fontSize: 15, marginBottom: 20,fontStyle:"italic" }}> - {book.creator}</Text>
+
+            <Pressable onPress={() => console.log("test")}>
+              <Text>Read</Text>
+            </Pressable>
 
 
             <Text style={{ fontSize:18,color:theme.background == "#F2EAC5" ? theme.background: theme.text,fontWeight:700,marginBottom:5 }}>Description</Text>
