2581a41ccf8efbebdb741246c958f2091ec218ae
kngval <bkvalteocs@gmail.com>
2025-04-28 10:53:29 +0800

    loading spinner

--- app/books/reading/[id].tsx
+++ app/books/reading/[id].tsx
@@ -1,33 +1,53 @@
 import { useLibrary } from "@/books/BookContext";
-import { TBookMetaData } from "@/types/book.types";
+import { useTheme } from "@/theme/themeContext";
 import { getBookData } from "@/utils/getBookData";
-import Epub from "epubjs";
 import { useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
-import { ScrollView } from "react-native";
+import { ActivityIndicator, ScrollView, View } from "react-native";
 
 export default function ReadingScreen() {
+  const [loadingBook, setLoadingBook] = useState<boolean>(false);
   const { library } = useLibrary();
+  const { theme } = useTheme();
   const { id } = useLocalSearchParams();
 
   useEffect(() => {
     loadBook();
-  },[])
-  const loadBook = async() => {
-    const book = library.find(b => b.id == id);
-    if (book) {
-      if(book.path){
-        const newBook = await getBookData(book.path)
-        const metadata = await newBook.loaded.metadata;
-        console.log("New Book Title : " ,metadata.title);
-      }
+  }, [])
+  const loadBook = async () => {
+    try {
+      setLoadingBook(true);
+      const book = library.find(b => b.id == id);
+      console.log("Passed Book : ", book?.title)
+      if (book) {
+        if (book.path) {
+          const newBook = await getBookData(book.path)
+          const metadata = await newBook.loaded.metadata;
+          console.log("New Book Title : ", metadata.title);
+        }
 
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoadingBook(false);
     }
 
   }
+  if (loadingBook) {
+    return (
+      <View style={{
+        flexDirection: "row",
+        width: "100%",
+        height: "90%",
+        justifyContent: "center",
+      }}>
+        <ActivityIndicator size={46} color={theme.text} />
+      </View>
+    )
+  }
   return (
-    <ScrollView>
-
+    <ScrollView style={{ height: "100%" }}>
     </ScrollView>
   )
 }
