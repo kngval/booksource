e4a61df30074691c52226f3ce97575c3453a084b
kngval <bkvalteocs@gmail.com>
2025-05-02 20:15:26 +0800

    extract zip & copy opfpath

--- app/books/reading/[id].tsx
+++ app/books/reading/[id].tsx
@@ -1,10 +1,11 @@
 import { useLibrary } from "@/books/BookContext";
 import { useTheme } from "@/theme/themeContext";
-import { getBookData } from "@/utils/getBookData";
 import { useLocalSearchParams } from "expo-router";
+import JSZip from "jszip";
 import { useEffect, useState } from "react";
 import { ActivityIndicator, View } from "react-native";
-import { WebView } from "react-native-webview";
+import * as FileSystem from "expo-file-system";
+import { XMLParser } from "fast-xml-parser";
 export default function ReadingScreen() {
   const [loadingBook, setLoadingBook] = useState<boolean>(false);
   const [bookHtml, setBookHtml] = useState<string | null>(null);
@@ -22,10 +23,12 @@ export default function ReadingScreen() {
       console.log("Passed Book : ", book?.title)
       if (book) {
         if (book.path) {
-          const bookData = await getBookData(book.path)
-          await bookData.ready;
-        }
+          const unzippedEpub = await unzipEpub(book.path);
+          const opfPath = await getOpfPath(unzippedEpub);
+          console.log("Opf : ",opfPath);
 
+
+        }
       }
     } catch (err) {
       console.error(err);
@@ -34,6 +37,32 @@ export default function ReadingScreen() {
     }
 
   }
+
+  const unzipEpub = async(epubUri : string):Promise<JSZip> => {
+    const epubBinary = await FileSystem.readAsStringAsync(epubUri,{
+      encoding : FileSystem.EncodingType.Base64,
+    })
+    const zip = await JSZip.loadAsync(epubBinary,{base64 : true})
+    return zip;
+  }
+
+  const getOpfPath = async(zip:JSZip):Promise<string> => {
+    const containerXml =  await zip.file('META-INF/container.xml')?.async('text');
+    console.log("Container XML : " ,containerXml);
+    if(!containerXml) throw new Error("container.xml not found");
+    const parser = new XMLParser({
+      ignoreAttributes: false,
+      attributeNamePrefix:'@_',
+      processEntities : true,
+      ignoreDeclaration: true,
+      removeNSPrefix:true
+    });
+    const xml = parser.parse(containerXml);
+    console.log("XML : " ,xml);
+    return xml.container.rootfiles.rootfile['@_full-path'];
+  }
+
+
   if (loadingBook) {
     return (
       <View style={{
@@ -47,32 +76,6 @@ export default function ReadingScreen() {
     )
   }
   return (
-    <WebView
-      originWhitelist={['*']}
-      source={{ html: bookHtml ? generateHTML(bookHtml) : "<p>Loading Book...</p>" }}
-      javaScriptEnabled={true}
-      domStorageEnabled={true}
-      allowFileAccess={true}
-      allowUniversalAccessFromFileURLs={true}
-    />
+   <View></View> 
   )
 }
-const generateHTML = (bookHtml: string) => `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>EPUB Reader</title>
-  <style>
-    html, body { margin: 0; padding: 0; height: 100%; }
-    #viewer { height: 100%; }
-  </style>
-</head>
-<body>
-  <div id="viewer">${bookHtml}</div>
-<script>
-console.log("webview JS");
-</script>
-</body>
-</html>
-`
